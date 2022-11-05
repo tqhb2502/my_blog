b0c253f19a4ecf790496564bc2c4ac018c2bcffa
tqhb2502 <84978747+tqhb2502@users.noreply.github.com>
2022-11-05 22:15:43 +0700

    _id auto increment

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
+    _id: { type: Number },
     name: { type: String, required: true },
     description: { type: String },
     videoID: { type: String, required: true },
     image: { type: String },
     slug: { type: String, slug: 'name', unique: true },
 }, {
+    _id: false,
     timestamps: true,
 });
 
@@ -27,6 +30,7 @@ Course.query.sortable = function (req) {
 
 // add plugin
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: "all"
